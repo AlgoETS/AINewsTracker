0ea1d446ef1df3ebd580d80367ce6645178fdbc7
Antoine Boucher <antoine.bou13@gmail.com>
2023-06-06 22:52:32 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -2,12 +2,14 @@ from fastapi import FastAPI
 
 # import all routers
 from .routers import users
+from .routers import companies
 
 # init app
 app = FastAPI()
 
 # add all routers to app
 app.include_router(users.router)
+app.include_router(companies.router)
 
 
 # this is command to run the server : uvicorn app.main:app --reload~
