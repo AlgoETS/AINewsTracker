5ab5024c33baa91c5b3d45e399cd4c935a2754f0
Antoine Boucher <antoine.boucher.1@ens.etsmtl.ca>
2023-06-10 10:12:57 -0400

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -5,13 +5,18 @@ from datetime import datetime
 from typing import List
 import os
 
-# load env variables
-load_dotenv()
 
-# Connect to MongoDB
-client = MongoClient("mongodb://localhost:27017/")
-db = client["AINewsTracker"]
-collection_names = ["companies", "articles", "scores", "rss_feeds"]
-collections = {name: db[name] for name in collection_names}
+def connect_to_db():
+    # Load environment variables
+    load_dotenv()
 
+    # Connect to MongoDB
+    client = MongoClient("mongodb://localhost:27017/")
+    db = client["AINewsTracker"]
 
+    return db
+
+def create_collections(db, collection_names: List[str]):
+    collection_names = ["companies", "articles", "scores", "rss_feeds"]
+    collections = {name: db[name] for name in collection_names}
+    return collections
