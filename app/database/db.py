ca08dd8b9cc8b778b517c5b95034466358a29624
Antoine Boucher <antoine.boucher.1@ens.etsmtl.ca>
2023-06-09 22:48:08 -0400

    Update db.py

--- app/database/db.py
+++ app/database/db.py
@@ -1,21 +1,17 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+from pydantic import BaseModel
+from datetime import datetime
+from typing import List
 import os
 
 # load env variables
 load_dotenv()
 
 # Connect to MongoDB
-client = MongoClient(os.getenv("Password"))
-
-# database
-db = client['AI_News_Tracker']
-
-# gettting collection
-collection_users = db['users']
-collection_company = db['company']
-collection_news = db['news']
-collection_news_sentiment = db['news_sentiment']
-collection_news_sentiment_score = db['news_sentiment_score']
+client = MongoClient("mongodb://localhost:27017/")
+db = client["AINewsTracker"]
+collection_names = ["companies", "articles", "scores", "rss_feeds"]
+collections = {name: db[name] for name in collection_names}
 
 
